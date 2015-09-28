9a73375c76b51c36ed680cab12c7e71f43d2869d
Michael Fich <michael@michaelfich.com>
2015-09-27 23:45:23 -0400

    Added an index and show view for Messages in the Administrator control panel.

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,2 +1,12 @@
 class Admin::MessagesController < ApplicationController
+  layout "administrator"
+  before_filter :is_admin?
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
 end
