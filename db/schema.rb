313999d6596389d55240c91b364a4d7cdb93339e
Michael Fich <michael@michaelfich.com>
2015-09-27 23:50:08 -0400

    Added a boolean 'read' field to the Message model to represent whether an administrator has read a particular message.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928031901) do
+ActiveRecord::Schema.define(version: 20150928034844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,9 +52,10 @@ ActiveRecord::Schema.define(version: 20150928031901) do
     t.string   "name"
     t.string   "email"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.text     "ip_address"
+    t.boolean  "read",       default: false
   end
 
   create_table "projects", force: :cascade do |t|
