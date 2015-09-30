2b39db4df6eeef68dd53025281be3725232b614e
Michael Fich <michael@michaelfich.com>
2015-09-30 01:09:35 -0400

    Added the new ENV fields to application.yml.sample to better assist with deployment.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @projects = Project.where(featured: true)
+    @projects = Project.order(id: :desc).where(featured: true).limit(3)
     @blogs = Blog.order(published_at: :desc).limit(2)
   end
 end
