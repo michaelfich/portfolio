9f13613c88866a7f183eead465f216025971c7d9
Michael Fich <michael@michaelfich.com>
2015-10-02 20:28:08 -0400

    Update rbenv ruby version to 2.2.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :deploy_to, '/home/deploy/portfolio'
 set :scm, :git
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.1'
+set :rbenv_ruby, '2.2.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
