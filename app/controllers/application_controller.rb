71f80ec063c56cbdcc86a46ab769e62c56f2fe48
Michael Fich <michael@michaelfich.com>
2015-10-07 23:05:43 -0400

    Added recent blog posts to footer, refactored content from the home page to reduce SQL queries, and updated footer styling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :recent_blogs
 
   def is_admin?
     if current_user
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
     else
       redirect_to auth_at_provider_path("github")
     end
+  end
+
+  def recent_blogs
+    @recent_blogs = Blog.order(published_at: :desc).limit(6)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
     @projects = Project.order(id: :desc).where(featured: true).limit(3)
-    @blogs = Blog.order(published_at: :desc).limit(2)
   end
 end
