bf35bfa9da8693a8df57751f3e099d6a1822e80e
Michael Fich <michael@michaelfich.com>
2015-10-08 04:48:05 -0400

    Added a 'published' field to blog posts, and request validation for the Blog show action.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,16 @@
 class BlogsController < ApplicationController
   def index
-    @blogs = Blog.all.order(published_at: :desc).page(params[:page]).per(5)
+    @blogs = Blog.where(published: true).order(published_at: :desc).page(params[:page]).per(5)
   end
 
   def show
     @blog = Blog.find_by(slug: params[:slug])
-    @comments = Comment.where(blog_id: @blog.id)
-    @comment = Comment.new
+    if @blog && @blog.published
+      @comments = Comment.where(blog_id: @blog.id)
+      @comment = Comment.new
+    else
+      flash[:alert] = "Unable to find the requested blog post."
+      redirect_to blogs_url
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008073721) do
+ActiveRecord::Schema.define(version: 20151008083507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,9 +31,10 @@ ActiveRecord::Schema.define(version: 20151008073721) do
     t.text     "body"
     t.integer  "user_id"
     t.datetime "published_at"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.string   "slug"
+    t.boolean  "published",    default: false
   end
 
   add_index "blogs", ["user_id"], name: "index_blogs_on_user_id", using: :btree
