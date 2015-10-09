8b5727cf65ec806b00cff6a353bca1d180201129
Michael Fich <michael@michaelfich.com>
2015-10-09 07:37:49 -0400

    Replaced previous attachment migration with one which is more accurately defined using the Paperclip generator.

--- db/migrate/20151008233157_add_image_to_projects.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToProjects < ActiveRecord::Migration
-  def change
-    add_attachment :projects, :image
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008233157) do
+ActiveRecord::Schema.define(version: 20151009113709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
