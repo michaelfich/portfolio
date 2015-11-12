16af4e3b6a191127622bd77a862b10af5b042e64
Michael Fich <michael@michaelfich.com>
2015-11-12 16:39:10 -0500

    Updated the User table to remove the null constraint on 'email' as some users were unable to sign up with their email privacy settings enabled on Github.

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -2,26 +2,23 @@ class OauthsController < ApplicationController
   skip_before_filter :require_login
 
   def oauth
+    # session[:return_to_url] = request.referer unless request.referer =~ /oauth/
     login_at(params[:provider])
   end
 
   def callback
     provider = auth_params[:provider]
     if @user = login_from(provider)
-      # redirect_back_or_to root_path, success: "Logged in from #{provider.titleize}!"
-      render "shared/login"
+      redirect_back_or_to root_path, success: "Logged in from #{provider.titleize}!"
     else
       begin
         @user = create_from(provider)
-        @user.admin = User.count == 1
-        @user.save
+        binding.pry
         reset_session
         auto_login(@user)
-        render "shared/login"
-        # redirect_back_or_to root_path, success: "Registered from #{provider.titleize}!"
+        redirect_back_or_to root_path, success: "Registered from #{provider.titleize}!"
       rescue
-        render "shared/login"
-        # redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
+        redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111044317) do
+ActiveRecord::Schema.define(version: 20151112213315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 20151111044317) do
   create_table "users", force: :cascade do |t|
     t.string   "login"
     t.string   "name"
-    t.string   "email",                      null: false
+    t.string   "email"
     t.string   "avatar"
     t.boolean  "admin",      default: false
     t.datetime "created_at"
