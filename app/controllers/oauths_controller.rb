c79c0d4d8eefbe9198c2e74474b8af7aa10f4147
Michael Fich <michael@michaelfich.com>
2015-11-12 14:09:28 -0500

    Added testing code to troubleshoot errors with non-administrator logins in production.

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,17 +8,20 @@ class OauthsController < ApplicationController
   def callback
     provider = auth_params[:provider]
     if @user = login_from(provider)
-      redirect_back_or_to root_path, success: "Logged in from #{provider.titleize}!"
+      # redirect_back_or_to root_path, success: "Logged in from #{provider.titleize}!"
+      render "shared/login"
     else
       begin
         @user = create_from(provider)
-        @user.admin = (User.count == 1)
+        @user.admin = User.count == 1
         @user.save
         reset_session
         auto_login(@user)
-        redirect_back_or_to root_path, success: "Registered from #{provider.titleize}!"
+        render "shared/login"
+        # redirect_back_or_to root_path, success: "Registered from #{provider.titleize}!"
       rescue
-        redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
+        render "shared/login"
+        # redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
       end
     end
   end
