ad97a55a060725378a501a433d4c40ddb35e1817
Michael Fich <michael@michaelfich.com>
2015-12-01 18:25:17 -0500

    Added a syntax highlighter for code blocks in the Blog.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  class HTML_Highlighter < Redcarpet::Render::HTML
+    def block(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+
   def markdown(text)
     render_options = {
       filter_html: true,
@@ -8,14 +14,16 @@ module ApplicationHelper
     }
 
     markdown_options = {
+      autolink: true,
       no_intra_emphasis: true,
-      highlight: true,
-      space_after_headers: true,
-      disable_indented_code_blocks: false,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true,
       underline: true
     }
 
-    renderer = Redcarpet::Render::HTML.new(render_options)
+    renderer = HTML_Highlighter.new(render_options)
     Redcarpet::Markdown.new(renderer, markdown_options).render(text).html_safe
   end
 end
